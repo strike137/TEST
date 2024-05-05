1e87db8365fb2849bfcf7d4f6758724a8f485770
SoyabeanPaste <67820382+strike137@users.noreply.github.com>
2024-05-05 15:25:45 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ def main():
   sum = 0
   numbers = eval(input("Enter a space-separated list of numbers: "))
   for num in numbers:
-    sum = sum + num
+    sum = sum + nu
   print(f"Sum of {numbers} = {sum}")
 main(
